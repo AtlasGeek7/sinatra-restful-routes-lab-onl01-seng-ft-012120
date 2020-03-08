bf8637e5c14256abea7d3b33a1881e9ab53d415d
Khalid T <khalid.tissire@gmail.com>
2020-03-08 15:04:11 +0000

    Automatically backed up by Learn

--- db/migrate/20200308150242_create_recipes_table.rb
+++ db/migrate/20200308150242_create_recipes_table.rb
@@ -2,7 +2,7 @@ class CreateRecipesTable < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :ingredientshttps://learn.co/tracks/online-software-engineering-structured/sinatra/activerecord/sinatra-restful-routes-lab#
+      t.string :ingredients
       t.string :cook_time
     end
   end
